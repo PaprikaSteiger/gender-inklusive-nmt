bd8b277d1b507a96757ec22d6eaae2c5e06dfc3a
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-10-15 17:56:08 +0200

    script to download europarl and delete english version

--- load_corpus.py
+++ load_corpus.py
@@ -2,11 +2,34 @@
 Loading the Europarl Corpus en-de and en-fr from huggingface
 """
 from pathlib import Path
+import requests
+import tarfile
 
-from datasets import load_dataset
+DIR: Path = (Path(__file__).parent / "datasets").mkdir(exist_ok=True) or Path(__file__).parent / "datasets"
+
+# europarl-mono at https://huggingface.co/datasets/mulcyber/europarl-mono/blob/main/europarl-mono.py
 
 def load_europarl_en_de():
-    return load_dataset("europarl_bilingual", lang1="en", lang2="de")
+    dataset = load_dataset("mulcyber/europarl-mono", "europarl-de")
+    return dataset
+
 
 def load_europarl_en_fr():
-    return load_dataset("europarl_bilingual", lang1="en", lang2="fr")
+    dataset = load_dataset("europarl_bilingual", lang1="en", lang2="fr")
+    return dataset
+
+
+def write_dataset_to_file(dirname: str, url: str):
+    response = requests.get(url, stream=True)
+    file = tarfile.open(fileobj=response.raw, mode="r|gz")
+    file.extractall(path=dirname)
+
+
+if __name__ == "__main__":
+    #write_dataset_to_file(dirname=DIR, url=r"https://www.statmt.org/europarl/v7/de-en.tgz")
+    write_dataset_to_file(dirname=DIR, url=r"https://www.statmt.org/europarl/v7/fr-en.tgz")
+    for file in DIR.iterdir():
+        if str(file).endswith(".en"):
+            file.unlink()
+
+
