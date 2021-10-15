a667b51ce437f419265d9df30c248cef59b70f50
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-10-15 11:42:32 +0200

    loading datasets from huggingface

--- load_corpus.py
+++ load_corpus.py
@@ -1,3 +1,12 @@
 """
 Loading the Europarl Corpus en-de and en-fr from huggingface
 """
+from pathlib import Path
+
+from datasets import load_dataset
+
+def load_europarl_en_de():
+    return load_dataset("europarl_bilingual", lang1="en", lang2="de")
+
+def load_europarl_en_fr():
+    return load_dataset("europarl_bilingual", lang1="en", lang2="fr")
