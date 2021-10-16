08eb902613b892c64892f3fa92e26f98506d1edb
Melvin Steiger <melvinsamson.steiger@uzh.ch>
2021-10-16 18:27:03 +0200

    huggingface throws error, download from europarl website

--- load_corpus.py
+++ load_corpus.py
@@ -10,15 +10,13 @@ DIR: Path = (Path(__file__).parent / "datasets").mkdir(exist_ok=True) or Path(__
 
 # TODO: Seemingly, the hugging face interface throws an error
 # europarl-mono at https://huggingface.co/datasets/mulcyber/europarl-mono/blob/main/europarl-mono.py
-def load_europarl_en_de():
-    dataset = load_dataset("mulcyber/europarl-mono", "europarl-de")
-    return dataset
-
-
-# TODO: Seemingly, the hugging face interface throws an error
-def load_europarl_en_fr():
-    dataset = load_dataset("europarl_bilingual", lang1="en", lang2="fr")
-    return dataset
+# def load_europarl_en_de():
+#     dataset = load_dataset("mulcyber/europarl-mono", "europarl-de")
+#     return dataset
+# # TODO: Seemingly, the hugging face interface throws an error
+# def load_europarl_en_fr():
+#     dataset = load_dataset("europarl_bilingual", lang1="en", lang2="fr")
+#     return dataset
 
 
 def write_dataset_to_file(dirname: str, url: str):
